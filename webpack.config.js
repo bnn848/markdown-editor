bc89c141912dffb0c8793b9403f0a922b740d2c2
bnn848 <77334083+bnn848@users.noreply.github.com>
2021-06-10 18:49:00 +0900

    webpack導入

--- webpack.config.js
+++ webpack.config.js
@@ -17,12 +17,25 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'), // path.resolve : 絶対パスに変換する
     filename: 'index.js',
     publicPath: 'dist/',
+  },
+  devServer: {
+    hot: true,
+    open: true,
   }
 }
-// module: {
-//   rules: [ // ビルド時に追加で行う処理一覧
-//     test: /\.ts$/, // <-- .ts$: 末尾に.tsとつくパス
-//     use: 'ts-loader', // 実行する処理内容
-//     exclude: /node_modules/, // 除外するファイル(node_moduleディレクトリ内のファイルは開発用なのでビルドする必要がない)
-//   ],
-// },
+
+/*
+module: {
+  rules: [ // ビルド時に追加で行う処理一覧
+    test: /\.ts$/, // <-- .ts$: 末尾に.tsとつくパス
+    use: 'ts-loader', // 実行する処理内容
+    exclude: /node_modules/, // 除外するファイル(node_moduleディレクトリ内のファイルは開発用なのでビルドする必要がない)
+  ],
+},
+
+devServer: { <--- npm i webpack-dev-serverしてのち追加
+  hot: true, <--- ファイル変更を自動的に反映
+  open: true, <--- 起動時にブラウザで開く
+}
+package.json - script -start に webpack-dev-serverを登録
+*/

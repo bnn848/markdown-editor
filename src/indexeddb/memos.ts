c22a4b96b07323ab426e770690e8ca784024ff83
bnn848 <77334083+bnn848@users.noreply.github.com>
2021-06-14 00:40:16 +0900

    第４章まで

--- src/indexeddb/memos.ts
+++ src/indexeddb/memos.ts
@@ -1,6 +1,7 @@
 import Dexie from 'dexie';
 import dayjs from 'dayjs';
 
+
 /* Interface TypeScriptで型定義する */
 export interface MemoRecord {
   datetime: string
@@ -8,20 +9,42 @@ export interface MemoRecord {
   text: string
 };
 
+
 /* Dexieインスタンスを生成 */
 const database = new Dexie('markdown-editor'); // Dexie('DB名')
 database.version(1).stores({memos: '&datetime'}) // DBのversion管理.stores(使用するtableとインデックスとなるデータ名)
 const memos: Dexie.Table<MemoRecord, string> = database.table('memos'); // memosというTableを生成
 
+
 /* putMemo() : Tableに保存 */
 export const putMemo = async (title: string, text: string): Promise<void> => { // 引数で保存したいデータを受け取る
   const datetime = dayjs().toString(); // 日時を取得
   await memos.put({datetime, title, text}); // 3つの要素をmemosテーブルに保存
+};
+
+
+/* ページ管理 */
+const NUM_PER_PAGE: number = 10 // 1ページあたり10件表示する
+export const getMemoPageCount = async (): Promise<number> => {
+  const totalCount = await memos.count(); // 全メモ数をmemosテーブルから取得する.memosはasyncでDBからデータを取得している
+  const pageCount = Math.ceil(totalCount / NUM_PER_PAGE) // ページ数: 小数点切り上げ
+
+  return pageCount > 0 ? pageCount : 1
 }
+// if (pageCount > 0) {
+//   return pageCount;
+// } else {
+//   return 1;
+// }
+
 
 /* getMemo() : Tableから取得 */
-export const getMemos = (): Promise<MemoRecord[]> => { // 戻り値の型定義、Promise型の配列
+export const getMemos = (page: number): Promise<MemoRecord[]> => { // 戻り値の型定義、Promise型の配列
+  const offset = (page - 1) * NUM_PER_PAGE // ページの先頭になるmemoを取得する
+  
   return memos.orderBy('datetime') // memosテーブルから、datetimeを昇順で取得
               .reverse() // 降順に並び替え
+              .offset(offset) // 取得するリスト内の開始位置を設定する。ex. page(3) === 30~39件目を表示
+              .limit(NUM_PER_PAGE) // 
               .toArray() // 配列に変換
 };
--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -5,6 +5,7 @@ import {
 } from 'react-router-dom';
 import { Header } from '../components/header';
 import {
+  getMemoPageCount,
   getMemos,
   MemoRecord // interfaceもimportすることができる
 } from '../indexeddb/memos'
@@ -23,14 +24,30 @@ History
 // export const History: React.FC<Props> = (props) => {
 export const History: React.FC<Props> = (props) => {
 
+  /* 状態管理 */
   const {setText} = props // propsからsetTextを分割
   const [memos, setMemos] = useState<MemoRecord[]>([]) // 型指定のあるuseStateで初期値は空配列
+  const [page, setPage] = useState(1) // page管理（初期値は１ページ目）
+  const [maxPage, setMaxPage] = useState(1) // memoの件数からページ数を計算する必要がある
   const history = useHistory()
 
   /* useEffectで初回レンダリング時に履歴を取得してsetState */
   useEffect(() => {
-    getMemos().then(setMemos)
+    getMemos(1).then(setMemos) // 1ページ目のmemoを取得、取得成功を待ってReactで描画
+    getMemoPageCount().then(setMaxPage) // page数も同様
   },[]);
+  
+  /* ページ管理 */
+  const canNextPage: boolean = page < maxPage; // Navi
+  const canPrevPage: boolean = page > 1;
+  const movePage = (targetPage: number) => { // 現在のページをtargetPageとして受け取る
+    if (targetPage < 1 || maxPage < targetPage) {
+      return
+    } else {
+      setPage(targetPage)
+      getMemos(targetPage).then(setMemos)
+    }
+  }
   
   return (
     <>
@@ -55,6 +72,21 @@ export const History: React.FC<Props> = (props) => {
           )
         })}
       </Wrapper>
+      <Paging>
+        <PagingButton
+          onClick={() => movePage(page - 1)}
+          disabled={!canPrevPage}
+        >
+          ＜
+        </PagingButton>
+        {page} / {maxPage}
+        <PagingButton
+          onClick={() => movePage(page + 1)}
+          disabled={!canNextPage}
+        >
+          ＞
+        </PagingButton>
+      </Paging>
     </>
   )
 };
@@ -75,6 +107,7 @@ const Wrapper = styled.div`
   right: 0;
   top: 3rem;
   padding: 0 1rem;
+  overflow-y: scroll;
 `
 const Memo = styled.button`
   display: block;
@@ -96,4 +129,27 @@ const MemoText = styled.div`
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
+`
+
+const Paging = styled.div`
+  bottom: 0;
+  height: 3rem;
+  left: 0;
+  line-height: 2rem;
+  padding: 0.5rem;
+  position: fixed;
+  right: 0;
+  text-align: center;
+`
+
+const PagingButton = styled.button`
+  background: none;
+  border: none;
+  display: inline-block;
+  height: 2rem;
+  padding: 0.5rem 1rem;
+
+  &:disabled {
+    color: silver;
+  }
 `
